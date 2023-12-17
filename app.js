affca24b6255bad70e60b7ed0ca4187fd2a4fe60
Vincent <122125309+vincentkho67@users.noreply.github.com>
2023-12-17 15:01:37 +0700

    init cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const router = require('./routes/index')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 app.use(router)
 
 app.listen(port, () => {
