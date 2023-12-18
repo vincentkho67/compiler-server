7c86c298f365d86d164e943e995d77f2922b60f2
Vincent <122125309+vincentkho67@users.noreply.github.com>
2023-12-18 11:37:49 +0700

    Fix formatting in sumArray display

--- seeders/20231217081214-initAssignment.js
+++ seeders/20231217081214-initAssignment.js
@@ -17,7 +17,7 @@ module.exports = {
       name: 'Array Mapping',
       context: 'const sumArray = (input) => {}',
       test_cases: 'const testCases = (code) => { const input = [1,2,3,4,5]; const expected = 15; const result = code(input); if (result === expected) { return true } else { return false } }; testCases(sumArray);',
-      display: 'const sumArray = (input) => {//write your solution here}',
+      display: 'const sumArray = (input) => {\n//write your solution here\n}',
       description: 'Given an array of numbers, sum all the numbers in the array and return the sum',
       createdAt: new Date(),
       updatedAt: new Date()
